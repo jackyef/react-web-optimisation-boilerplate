f39ec4b039a18113eaf943ef5dd1928a43da81fd
Jacky Efendi <jacky.efendi1@gmail.com>
2019-09-10 13:38:32 +0700

    chore: rework app structure

--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { object } from 'prop-types';
 import { Route, Switch } from 'react-router';
-import { Router } from 'react-router-dom';
 
 import { HomeView } from './Home';
 import Header from '../components/Header';
 
 import './styles.js';
 
-const Routes = ({ history }) => {
+const Routes = () => {
   return (
-    <Router history={history}>
       <>
         <Header />
         <Switch>
           <Route exact path="/" component={HomeView} />
         </Switch>
       </>
-    </Router>
   );
 };
 
